4bfdc8b49e65599c772bea94b39628fec0f7699f
Md Irfan <irfanjunaid78081@gmail.com>
2022-11-06 21:34:27 +0530

    added the cross origin

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ require("dotenv").config();
 const connection = require("./config/db");
 const { blogRouter } = require("./Routes/blog.route");
 const { userRouter } = require("./Routes/user.route");
+const cors = require("cors");
 
+const PORT = process.env.PORT || 5000;
 const app = express();
-const PORT = process.env.PORT || 5000
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
+  res.send("Api homepage");
+});
 
-    res.send('Api homepage')
-})
+app.use("/api/user", userRouter);
 
-app.use('/api/user', userRouter)
-
-app.use('/api/blog', blogRouter)
-
+app.use("/api/blog", blogRouter);
 
 app.listen(PORT, async () => {
   try {
